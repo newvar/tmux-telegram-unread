25d656168f39a2fe5b0d262609ba6f637582d6e2
newvar <newvar@gmail.com>
2015-03-07 00:56:26 +0300

    +

--- scripts/telegram_unread.sh
+++ scripts/telegram_unread.sh
@@ -5,8 +5,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/shared.sh"
 
 print_telegram_unread() {
-	if [ command_exists "telegram-cli" ]; then
-        telegram-cli -NWl1 -e 'dialog_list' | grep -c '[1-9][0-9]* unread'
+	if command_exists "telegram-cli"; then
+        telegram-cli -e 'dialog_list' | grep -c '[1-9][0-9]* unread'
     else
         echo "Install Telegram CLI"
 	fi
